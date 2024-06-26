a9d0847e42a2180c5a224dbabd1565e920279b91
Illyes <illyeszerga645@gmail.com>
2024-06-26 17:07:16 +0200

    feat: Connected the app to the bot on discord

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const {Client, Events, GatewayIntentBits} = require('discord.js')
 const {config} = require('dotenv')
 config();
-
 const token = process.env.DISCORD_TOKEN
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Connecté sur le login ${client.user.tag}`)
+})
+
+client.login(token)
