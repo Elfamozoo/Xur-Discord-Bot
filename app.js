084a6adc1754731e68e46522c82bee1013ee51db
Illyes <illyeszerga645@gmail.com>
2024-06-26 18:18:59 +0200

    feat: Add a folder "commands" for the creation of all the commands that I will need and added code to load these files on startup in app.js

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-const {Client, Events, GatewayIntentBits} = require('discord.js')
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, Events, GatewayIntentBits} = require('discord.js')
 const {config} = require('dotenv')
 config();
 const token = process.env.DISCORD_TOKEN
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[ALERTE] Il manque à la commande ${filePath} une propriété "data" ou "execute" requise.`);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 client.once(Events.ClientReady, readyClient => {
     console.log(`Connecté sur le login ${client.user.tag}`)
 })
+
 
 client.login(token)
