ee356c37dd100f919c6a89a0e21b3af2b81c4da7
Illyes <illyeszerga645@gmail.com>
2024-07-03 22:04:35 +0200

    feat: The slash command xur.js is finished and is working for the location of xur. MVP done. Will be adding some new features.

--- commands/utility/xur.js
+++ commands/utility/xur.js
@@ -1,10 +1,10 @@
 const {SlashCommandBuilder} = require("discord.js");
-
+const isXurAvailable = require("../../services/isXurAvailable")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('xur')
         .setDescription('Information sur la localisation de Xur.'),
     async execute(interaction) {
-        
+        await interaction.reply(await isXurAvailable())
     }
 }
