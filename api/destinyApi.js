211217b87e446b78aa0589bd32d7f6970a59bdc6
Illyes <illyeszerga645@gmail.com>
2024-07-03 19:19:36 +0200

    feat: Change in the API cause the endpoints was changing every week now i retrieve the endpoints directly from the manifest.

--- api/destinyApi.js
+++ api/destinyApi.js
@@ -3,21 +3,25 @@ const axios = require("axios");
 config();
 
 const token = process.env.DESTINY_TOKEN
-const vendorsDefinitionFrEndpoint = "https://www.bungie.net/common/destiny2_content/json/fr/DestinyVendorDefinition-c2aab5db-09a6-4170-85dd-91599475546b.json"
-const destinationDefinitionFrEndpoint = "https://www.bungie.net/common/destiny2_content/json/fr/DestinyDestinationDefinition-c2aab5db-09a6-4170-85dd-91599475546b.json"
-
+headerDestiny = "https://www.bungie.net/"
+xurId = "2190858386"
+const destinyManifest = 'https://www.bungie.net/Platform/Destiny2/Manifest/'
 
 // Avec Axios je fetch uniquement les informations qui m'intéresse du Json ici les infos de Xur.
 // Ensuite je recupere le hash de la localisation que j'utilise pour fetch les informations de la dite localisation.
 // Enfin je recupere le nom de la location via le 2eme appel api.
 run = async () => {
     try {
-        const vendorsResponse = await axios.get(vendorsDefinitionFrEndpoint)
-        const xurInfos = vendorsResponse.data['2190858386']
+        const manifestResponse = await axios.get(destinyManifest)
+        const vendorsDefinitionFrEndpoint = manifestResponse.data.Response.jsonWorldComponentContentPaths.fr.DestinyVendorDefinition
+        const destinationDefinitionFrEndpoint = manifestResponse.data.Response.jsonWorldComponentContentPaths.fr.DestinyDestinationDefinition
+
+        const vendorsResponse = await axios.get(`${headerDestiny}${vendorsDefinitionFrEndpoint}`)
+        const xurInfos = vendorsResponse.data[xurId]
         const locationHash = xurInfos.locations[0].destinationHash
 
-        const destinationResponse = await axios.get(destinationDefinitionFrEndpoint)
-        const xurLocation =  destinationResponse.data[locationHash]
+        const destinationResponse = await axios.get(`${headerDestiny}${destinationDefinitionFrEndpoint}`)
+        const xurLocation = destinationResponse.data[locationHash]
         const locationName = xurLocation.displayProperties.description
     } catch (e) {
         console.log(e)
--- commands/utility/xur.js
+++ commands/utility/xur.js
@@ -1,1 +1,10 @@
 const {SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('xur')
+        .setDescription('Information sur la localisation de Xur.'),
+    async execute(interaction) {
+
+    }
+}
